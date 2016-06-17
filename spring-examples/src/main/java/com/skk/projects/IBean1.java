58b82607f6d1fd633fe98a064a646b519b5b3919
suyashkkaulgud <kaulgud.suyash@gmail.com>
2016-06-17 17:13:43 +0000

    initial commit
    initial commit

---
+++ spring-examples/src/main/java/com/skk/projects/IBean1.java
@@ -0,0 +1,6 @@
+package com.skk.projects;
+
+public interface IBean1 {
+    String hello(String name);
+    void setVar1(String var1);
+}
---
+++ spring-examples/src/main/java/com/skk/projects/IRunner.java
@@ -0,0 +1,12 @@
+package com.skk.projects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: xaoc
+ * Date: 30.11.11
+ * Time: 17:22
+ */
+public interface IRunner {
+    void run();
+    void setBean1(IBean1 bean1);
+}
